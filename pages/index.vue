3384a1561f05ff0b725ae99b7d2db6e1eb4a4671
John Espiritu <hkri@protonmail.com>
2020-11-22 18:17:04 +0800

    Reduce size of landing page logo and name font size.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="relative flex justify-center h-full">
     <div class="text-center self-center">
-      <Logo width="120" height="120" style="fill:#131313" />
-      <h1 class="text-3xl uppercase">John Espiritu</h1>
+      <Logo width="80" height="80" style="fill:#191919" />
+      <h1 class="text-2xl uppercase">John Espiritu</h1>
       <div class="font-bold">Software Developer</div>
       <Menu />
     </div>

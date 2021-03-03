5919d6e690fbd12e1e44b3da8bb65da65c573911
johnillo <hkri@protonmail.com>
2021-03-04 01:02:32 +0800

    Remove duplicates and fix gallery layout.

---
+++ components/Gallery.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="gallery">
+    <div class="hidden sm:block mt-4">
+      <div class="mb-8 flex flex-wrap">
+        <div class="w-full sm:w-1/3 pr-2">
+          <a :href="url" v-for="(url, index) in col1" :key="index" target="_blank">
+            <img class="w-full mb-4 shadow-md" :src="url"/>
+          </a>
+        </div>
+        <div class="w-full sm:w-1/3 pl-2">
+          <a :href="url" v-for="(url, index) in col2" :key="index" target="_blank">
+            <img class="w-full mb-4 shadow-md" :src="url"/>
+          </a>
+        </div>
+        <div class="w-full sm:w-1/3 pl-2">
+          <a :href="url" v-for="(url, index) in col3" :key="index" target="_blank">
+            <img class="w-full mb-4 shadow-md" :src="url"/>
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="block sm:hidden mt-4">
+      <a :href="url" v-for="(url, index) in images" :key="index" target="_blank">
+        <img class="w-full mb-4 shadow-md" :src="url"/>
+      </a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.gallery img {
+  border-radius: 0.25em;
+  border: 5px solid white;
+}
+</style>
+
+<script>
+export default {
+  props: {
+    images: Array,
+  },
+  computed: {
+    col1 () {
+      return this.images.reduce((a, v, i) => {
+        if (i % 3 == 0) {
+          a.push(v)
+        }
+        return a
+      }, [])
+    },
+    col2 () {
+      return this.images.reduce((a, v, i) => {
+        if (i % 3 == 1) {
+          a.push(v)
+        }
+        return a
+      }, [])
+    },
+    col3 () {
+      return this.images.reduce((a, v, i) => {
+        if (i % 3 == 2) {
+          a.push(v)
+        }
+        return a
+      }, [])
+    },
+  },
+}
+</script>
--- pages/portfolio/artworks/index.vue
+++ pages/portfolio/artworks/index.vue
@@ -9,48 +9,21 @@
         Mostly drawn with charcoal or ink.
       </div>
     </div>
-    <div class="gallery mb-8 flex flex-wrap mt-4">
-      <div class="w-full md:w-1/2 pr-2">
-        <a :href="url" v-for="(url, index) in traditionalCol1" :key="index" target="_blank">
-          <img :src="url"/>
-        </a>
-      </div>
-      <div class="w-full md:w-1/2 pl-2">
-        <a :href="url" v-for="(url, index) in traditionalCol2" :key="index" target="_blank">
-          <img :src="url"/>
-        </a>
-      </div>
-    </div>
+    <Gallery :images="traditional"/>
     <div class="border-l-4 border-blue-600 pl-2">
       <h4 class="text-xl font-bold">Digital</h4>
       <div>
         Made with pen tablets (Wacom, Genius) and PaintTool SAI or Krita.
       </div>
     </div>
-    <div class="gallery mb-16 flex flex-wrap mt-4">
-      <div class="w-full md:w-1/2 pr-2">
-        <a :href="url" v-for="(url, index) in digitalCol1" :key="index" target="_blank">
-          <img :src="url"/>
-        </a>
-      </div>
-      <div class="w-full md:w-1/2 pl-2">
-        <a :href="url" v-for="(url, index) in digitalCol2" :key="index" target="_blank">
-          <img :src="url"/>
-        </a>
-      </div>
-    </div>
+    <Gallery :images="digital"/>
     <p class="text-gray-800" v-if="!hasArtworks">
       No items found.
     </p>
     <Footer/>
+    <ScrollToTop />
   </div>
 </template>
-
-<style scoped>
-.gallery img {
-  margin-bottom: 1em;
-}
-</style>
 
 <script>
 export default {
@@ -65,38 +38,6 @@ export default {
     return { title: this.title }
   },
   computed: {
-    digitalCol1 () {
-      return this.digital.reduce((a, v, i) => {
-        if (i % 2 == 0) {
-          a.push(v)
-        }
-        return a
-      }, [])
-    },
-    digitalCol2 () {
-      return this.digital.reduce((a, v, i) => {
-        if (i % 2 != 0) {
-          a.push(v)
-        }
-        return a
-      }, [])
-    },
-    traditionalCol1 () {
-      return this.traditional.reduce((a, v, i) => {
-        if (i % 2 == 0) {
-          a.push(v)
-        }
-        return a
-      }, [])
-    },
-    traditionalCol2 () {
-      return this.traditional.reduce((a, v, i) => {
-        if (i % 2 != 0) {
-          a.push(v)
-        }
-        return a
-      }, [])
-    },
     hasArtworks () {
       return this.traditional.length > 0 || this.digital.length > 0
     },
